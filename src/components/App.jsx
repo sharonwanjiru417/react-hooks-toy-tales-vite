3fad2f86ed7252901e7a95fffef19c4ba854f6b9
sharonwanjiru417 <sharonwanjiru417@gmail.com>
2026-05-14 09:40:34 +0300

    Toy tales

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,62 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then(res => res.json())
+      .then(data => setToys(data));
+  }, []);
 
   function handleClick() {
-    setShowForm((showForm) => !showForm);
+    setShowForm(showForm => !showForm);
+  }
+
+  function handleAddToy(newToy) {
+    fetch("http://localhost:3001/toys", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ ...newToy, likes: 0 }),
+    })
+      .then(res => res.json())
+      .then(toy => setToys(prev => [...prev, toy]));
+  }
+
+  function handleDeleteToy(id) {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "DELETE",
+    }).then(() => setToys(prev => prev.filter(toy => toy.id !== id)));
+  }
+
+  function handleLikeToy(id, currentLikes) {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ likes: currentLikes + 1 }),
+    })
+      .then(res => res.json())
+      .then(updatedToy => 
+        setToys(prev => prev.map(toy => toy.id === updatedToy.id ? updatedToy : toy))
+      );
   }
 
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onAddToy={handleAddToy} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer
+        toys={toys}
+        onDeleteToy={handleDeleteToy}
+        onLikeToy={handleLikeToy}
+      />
     </>
   );
 }
